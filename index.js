ebe426c727d38368578de94121f84788b7f23e63
klgibsonjr <klgibsonjr@gmail.com>
2023-01-12 23:08:28 -0800

    added switch statment for mainMenu() prompts

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('console.table');
 
 const PORT = process.env.PORT || 3000;
 
-const db = mysql.createConnection({
+const databaseConnection = mysql.createConnection({
   host: 'localhost',
   port: { PORT },
   user: process.env.DB_USER,
@@ -12,27 +12,63 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
-db.connect((err) => {
+databaseConnection.connect((err) => {
   if (err) throw err;
   mainMenu();
 });
 
 const mainMenu = () => {
-  return inquirer.prompt([
-    {
-      type: 'list',
-      name: 'main_menu',
-      message: 'What would you like to do? ',
-      choices: [
-        'View all departments',
-        'Add a department',
-        'View all employees',
-        'Add a new employee',
-        'View all roles',
-        'Add a role',
-        'Update employee role',
-        'Quit',
-      ],
-    },
-  ]);
+  return inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'main_menu',
+        message: 'What would you like to do? ',
+        choices: [
+          'View all departments',
+          'Add a department',
+          'View all employees',
+          'Add a new employee',
+          'View all roles',
+          'Add a role',
+          'Update employee role',
+          'Quit',
+        ],
+      },
+    ])
+    .then((response) => {
+      switch (response.mainMenu) {
+        case 'View alll departments':
+          viewAllDepartments();
+          break;
+
+        case 'Add a department':
+          addDepartment();
+          break;
+
+        case 'View all employees':
+          viewAllEmployees();
+          break;
+
+        case 'Add a new employee':
+          addEmployee();
+          break;
+
+        case 'View all roles':
+          viewAllRoles();
+          break;
+
+        case 'Add a role':
+          addRole();
+          break;
+
+        case 'Update employee role':
+          updateEmployeeRole();
+          break;
+
+        case 'Quit':
+          databaseConnection.end();
+          break;
+      }
+    });
 };
