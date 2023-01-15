0cdc025b5d110ee3691ede2b6022eeee900b9458
klgibsonjr <klgibsonjr@gmail.com>
2023-01-15 00:47:52 -0800

    completed adding values to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,15 +4,15 @@ VALUES ('Sales'),
        ('Finance'),
        ('Customer Service');
 
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES ('Sales Lead', 150000, 1),
       ('Salesperson', 75000, 1),
       ('Senior Software Engineer', 200000, 2),
       ('Software Engineer', 120000, 2),
       ('Senior Account', 150000, 3),
       ('Account', 100000, 3),
-      ('Customer Service Manager', 80000, 4),
-      ('Customer Service Representative', 65000, 4);
+      ('Service Manager', 80000, 4),
+      ('Service Representative', 65000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('Chino', 'Moreno', 1, NULL),
