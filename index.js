768396f79b2181dfbbb09c7bdff91a126a90a6a1
klgibsonjr <klgibsonjr@gmail.com>
2023-01-14 23:10:46 -0800

    worked on functionality for viewing and adding departments

--- index.js
+++ index.js
@@ -76,11 +76,55 @@ const mainMenu = () => {
 const viewAllDepartments = () => {
   console.log('Viewing all departments...\n');
 
-  let query = 'SELECT * FROM department';
+  const query = 'SELECT * FROM department';
 
   database.query(query, (err, res) => {
     if (err) throw err;
     console.table(res);
     mainMenu();
+  });
+};
+
+const viewAllEmployees = () => {
+  console.log('Viewing all employees...\n');
+
+  const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id)
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY employee.id;`;
+
+  database.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    mainMenu();
+  });
+};
+
+const viewAllRoles = () => {
+  console.log('Viewing all employee roles...\n');
+
+  const query = `SELECT role.title, role.id, role.department_id, role.salary;`;
+
+  database.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    mainMenu();
+  });
+};
+
+const addDepartment = () => {
+  database.query(`SELECT * FROM department;`, (err, res) => {
+    if (err) throw err;
+    console.log(err);
+
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'department',
+        choices: ['Sales', 'Engineering', 'Finance', 'Customer Service'],
+      },
+    ]);
   });
 };
